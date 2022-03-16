300a4136abe484b2a91dc8d6e5a86564151207b0
ABHINAVVP <abhinavvp026@gmail.com>
2022-03-16 21:52:12 +0530

    updated on 16-03-22

---
+++ te/learn/collections/set/DemoTreeSet2.java
@@ -0,0 +1,13 @@
+package com.te.learn.collections.set;
+
+import java.util.TreeSet;
+
+public class DemoTreeSet2 {
+
+	public static void main(String[] args) {
+		TreeSet<Student> treeSet=new TreeSet<Student>();
+		
+
+	}
+
+}
