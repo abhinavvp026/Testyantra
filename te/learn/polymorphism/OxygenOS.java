4b0cc7992b3c8130097201f5eb38f555f300379c
ABHINAVVP <abhinavvp026@gmail.com>
2022-03-17 09:09:16 +0530

    updated on 17-03-22

---
+++ te/learn/polymorphism/OxygenOS.java
@@ -0,0 +1,13 @@
+package com.te.learn.polymorphism;
+
+public final class OxygenOS extends Android {
+	@Override
+	public void runOS() {
+		System.out.println("This is advanced running");
+	}
+	public void security() {
+		
+	}
+
+	
+}
